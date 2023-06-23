0096972e4f1c4ede41dfb8052a0b02b421ae14c8
afnane2000 <afnanenaciri2000@gmail.com>
2023-06-23 11:23:27 +0100

    print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_alphabet - Entry point
+ *
+ *Description: 'the program's description'
+ *@void: describe the parameter
+ *
+ *Return: Always 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int c;
+	int i;
+
+for (i = 1 ; i < 11 ; i++)
+{
+	for (c = 'a' ; c <= 'z' ; c++)
+		_putchar(c);
+	_putchar('\n');
+}
+}
