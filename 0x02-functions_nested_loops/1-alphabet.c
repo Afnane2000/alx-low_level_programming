1492fcdf04220e25ee76ff43b36da92687579309
afnane2000 <afnanenaciri2000@gmail.com>
2023-06-23 10:20:09 +0100

    correction de erreurs

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *print_alphabet - Entry point
+ *
+ *Description: 'the program's description'
+ *@void: describe the parameter
+ *
+ *Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	int c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+		_putchar(c);
+	_putchar(\n');
+}
