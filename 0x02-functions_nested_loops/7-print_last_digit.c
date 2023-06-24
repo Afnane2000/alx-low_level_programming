c42ad0f423547aacd8568d032a5befe1a0c84ada
afnane2000 <afnanenaciri2000@gmail.com>
2023-06-24 14:28:31 +0100

    7 last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *print_last_digit - affiche last digit
+ *
+ *@i : variable local
+ *
+ *Return: returne valeur
+ *
+ */
+int print_last_digit(int i)
+{
+	return (i % 10);
+}
