4bec91e71e59c3136649a0a3d1e50174cd3d4018
afnane2000 <afnanenaciri2000@gmail.com>
2023-06-24 14:11:18 +0100

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_abs - calcule la valeur absolue
+ *
+ *@i : variable local
+ *abs : fonction valeur absolue
+ *Return: value
+ */
+int _abs(int i)
+{
+	return (abs(i));
+}
