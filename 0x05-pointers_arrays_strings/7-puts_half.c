6f30e1c851b526549b553e673e0c3a32ebefbe5b
afnane2000 <afnanenaciri2000@gmail.com>
2023-06-27 02:27:48 +0100

    0_reset

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *puts_half - function to swap the value
+ *
+ *
+ *@str : parametre pointeur
+ *
+ *
+ */
+void puts_half(char *str)
+{
+int l, i;
+
+	for (l = 0 ; str[l] != '\0' ; l++)
+		;
+
+	for (i = l / 2 ; str[i] != '\0' ; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
