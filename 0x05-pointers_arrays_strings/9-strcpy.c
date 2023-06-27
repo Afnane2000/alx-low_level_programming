050fee416e867b15a4855c440ff04f50b4bb6fe2
afnane2000 <afnanenaciri2000@gmail.com>
2023-06-27 17:47:51 +0100

    0_reset

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ **_strcpy - function to swap the value
+ *
+ *@dest : parametre pointeur
+ *@src : parametre pointeur
+ *
+ *Return: return pointeur.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	do {
+		dest[i] = src[i];
+		i++;
+	} while (src[i] != '\0');
+	return (dest);
+}
