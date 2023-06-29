0d5bd30f0c05d08c304f1e4f3fe6b84520e20f91
afnane2000 <afnanenaciri2000@gmail.com>
2023-06-29 17:17:50 +0100

    2-strc

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,27 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * main - check the code
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
  *
- * Return: Always 0.
+ * Return: dest
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
+	int j;
 
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
 }
